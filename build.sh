e2789540ccd7d860ba863dc3deba70f8ec0c389d
Lea Boyadjian <lb5255@rit.edu>
2022-02-01 21:57:41 -0500

    make new changes for hw3

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 echo "Cleaning existing classes..."
-rm -f *.class
+rm -f ./build/*.class
 # This command looks for matching files and runs the rm command for each file
 # Note that {} are replaced with each file name
 find . -name \*.class -exec rm {} \;
 
 echo "Compiling source code and unit tests..."
-javac -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar *.java
+javac -d ./build -cp lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar ./src/main/java/*.java ./src/test/java/*.java
 if [ $? -ne 0 ] ; then echo BUILD FAILED!; exit 1; fi
 
 echo "Running unit tests..."
@@ -14,4 +14,4 @@ java -cp .:lib/junit-4.12.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCo
 if [ $? -ne 0 ] ; then echo TESTS FAILED!; exit 1; fi
 
 echo "Running application..."
-java RunEdgeConvert
+java -cp .:build RunEdgeConvert
